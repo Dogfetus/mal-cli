f7217a75d2ece34601dd3f1e91a24afbf4d18af7
dogfetus <pokemonbolle@gmail.com>
2025-04-06 23:27:08 +0200

    add docker iamge for server

--- server/src/main.rs
+++ server/src/main.rs
@@ -67,7 +67,7 @@ fn main() {
 
     println!("Now listening on localhost:8000");
 
-    rouille::start_server("localhost:8000", move |request| {
+    rouille::start_server("0.0.0.0:8000", move |request| {
         router!(request,
             (GET) (/) => {
                 rouille::Response::redirect_302("/hello")
