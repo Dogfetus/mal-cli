93be8e83430632d513f1ebb40d60e54521d348f3
dogfetus <pokemonbolle@gmail.com>
2025-04-15 00:35:07 +0200

    add callback parameter to oauth function

--- src/mal/mod.rs
+++ src/mal/mod.rs
@@ -1,5 +1,22 @@
 mod oauth;
+use std::fs;
 
+//TODO: idk where to place this callback function 
+//TODO: should this be oauth login or just login with options?
+//TODO: encrypt the tokens somehow
+//TODO: check if the tokens exists before trying to login 
+//TODO: read the tokens to memory, and start using them to request data (using some mal api wrapper)
 pub fn init_oauth() {
-    oauth::oauth_login();
+    if !fs::metadata(".mal").is_ok() {
+        fs::create_dir(".mal").expect("Failed to create .mal directory");
+    }
+
+    oauth::oauth_login( |at, rt, ei| 
+        {
+            let data = format!("Access Token: \"{}\"\nRefresh Token: \"{}\"\nExpires In: \"{}\"", at, rt, ei);
+            fs::write(".mal/odata", data)?;
+            Ok(())
+        });
 }
+
+
--- src/mal/oauth.rs
+++ src/mal/oauth.rs
@@ -1,13 +1,13 @@
 #![allow(unreachable_code)]
-use ureq;
-use rouille::router;
-use anyhow::Result;
+use std::sync::mpsc::{self};
 use std::time::Duration;
 use rouille::try_or_400;
 use rouille::post_input;
-use std::sync::mpsc::{self};
+use rouille::router;
+use anyhow::Result;
 use std::thread;
-
+use open;
+use ureq;
 
 
 
@@ -16,15 +16,12 @@ const BACKEND_URL: &str = "https://mal-cli.dogfetus.no";
 
 
 
-//TODO: this might be moved to mod.rs instead (this function)
-//TODO: might change to sending a callback function to wait for redirect 
-//TODO: add thread that stops the server when its gotten the data (callback is called) 
-pub fn oauth_login() {
-    if let Some((port, joinable)) = start_callback_server() {
-
-        let url = get_oauth_url(port).expect("Can't connect to backend");
-        println!("url: {}", url);
-
+pub fn oauth_login<F>(callback: F) 
+where
+    F: FnOnce(String, String, String) -> Result<()> + Send + Sync + 'static + Copy
+{
+    if let Some((port, joinable)) = start_callback_server(callback) {
+        let _url = get_oauth_url(port).expect("Can't connect to backend");
         joinable.join().unwrap();
     } 
 }
@@ -37,16 +34,24 @@ fn get_oauth_url(port: u16) -> Result<String> {
         .send_form(body)?
         .body_mut()
         .read_to_string()?;
-    println!("get_oauth_url{}", url);
+    open::that(&url)
+        .expect("Failed to open browser");
+    println!("if the browser didn't open, visit this url: {}", url);
     Ok(url)
 }
 
 
+
 /* 
 * This function starts a local server to listen for the callback from the OAuth provider.
+* it takes a callback function as an argument, which will be called when the server receives a callback.
+* The callback function should accept three parameters: access_token, refresh_token, and expires_in.
 * It will return the port number on which the server is running.
 * */
-fn start_callback_server() -> Option<(u16, thread::JoinHandle<()>)> {
+fn start_callback_server<F>(callback: F) -> Option<(u16, thread::JoinHandle<()>)> 
+where
+    F: FnOnce(String, String, String) -> Result<()> + Send + Sync + 'static + Copy
+{
     let mut port: u16 = 53400;
     let (tx, rx) = mpsc::channel::<()>();
 
@@ -68,6 +73,13 @@ fn start_callback_server() -> Option<(u16, thread::JoinHandle<()>)> {
                         Ok(content) => content,
                         Err(_) => return rouille::Response::text("Failed to read template") 
                     };
+
+                    match callback(data.access_token, data.refresh_token, data.expires_in) {
+                        Ok(_) => {}, 
+                        Err(err) => {
+                            return rouille::Response::text(format!("Callback failed, error: {}", err));
+                        },
+                    }
 
                     let _ = _tx.send(()); 
                     rouille::Response::html(html_content)
